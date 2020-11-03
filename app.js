a255630394fda990818807a8219d16b7de228b20
wuky <zz3692177@yahoo.com.tw>
2020-11-03 17:41:29 +0800

    index

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+// routes setting
+app.get('/', (req, res) => {
+  // past the movie data into 'index' partial template
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  console.log(restaurant)
+  res.render('show', { restaurant: restaurant })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
